dbf5761639b235ba2bcbe9b8b0cdcebd71f18417
vd <vdinh2999@gmail.com>
2025-11-19 15:45:25 -0600

    History and Profile screens

--- HealthLens/app/(tabs)/_layout.tsx
+++ HealthLens/app/(tabs)/_layout.tsx
@@ -4,6 +4,8 @@ export default function TabLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="Diagnose" options={{ headerShown: false }} />
+      <Tabs.Screen name="History" options={{ headerShown: false }} />
+      <Tabs.Screen name="Profile" options={{ headerShown: false }} />
     </Tabs>
   );
 }
