06e4ecb297adcc20ca7d392c3f849b06f87186e5
Krish Gandhi <gandhikrish01@gmail.com>
2026-04-21 18:17:28 -0500

    Remove firebaseKey from repo

--- HealthLens/firebaseKey.ts
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAzzXusKz05PWfB8JqJOx3Fktlrfmodvx8",
-  authDomain: "healthlens-942ea.firebaseapp.com",
-  projectId: "healthlens-942ea",
-  storageBucket: "healthlens-942ea.firebasestorage.app",
-  messagingSenderId: "870686134158",
-  appId: "1:870686134158:web:f519df8cef501a2aa29edf",
-  measurementId: "G-2VFXXJ7MW3"
-};
